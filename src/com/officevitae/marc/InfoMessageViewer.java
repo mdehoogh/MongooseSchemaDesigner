35a0f2ca4559c71699e98cf4727d441d1ff7bb9d
mdehoogh <marc@officevitae.com>
2018-10-29 18:31:21 +0100

    info messages viewer and dealing with load exceptions 2

---
+++ src/com/officevitae/marc/InfoMessageViewer.java
@@ -0,0 +1,46 @@
+package com.officevitae.marc;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class InfoMessageViewer extends JPanel implements Utils.InfoMessageListener{
+
+	private Object source;
+	public Object getSource(){return source;}
+	public void infoMessagesChanged(){
+		infoMessagesTextArea.setText(String.join("\n",Utils.getInfoMessages(this.source)));
+	}
+	private JTextArea infoMessagesTextArea;
+	private JButton clearButton;
+	public InfoMessageViewer(){
+		super(new BorderLayout());
+		Utils.addInfoMessageListener(this); // listen in to any changes...
+		add(infoMessagesTextArea=new JTextArea());
+		infoMessagesTextArea.setEnabled(false);
+		JPanel buttonPanel=new JPanel();
+		buttonPanel.add(clearButton=new JButton("Clear"));
+		clearButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				Utils.removeInfoMessages(source);
+				infoMessagesTextArea.setText("");
+			}
+		});
+		clearButton.setEnabled(false);
+		add(buttonPanel,BorderLayout.SOUTH);
+	}
+	public InfoMessageViewer setSource(Object source){
+		Utils.removeInfoMessageListener(this);
+		this.source=source;
+		if(this.source!=null){
+			clearButton.setEnabled(this.source!=null&&Utils.hasInfoMessages(this.source));
+			infoMessagesTextArea.setText(String.join("\n",Utils.getInfoMessages(this.source)));
+			Utils.addInfoMessageListener(this); // inform me about any changes...
+		}else
+			infoMessagesTextArea.setText("");
+		return this;
+	}
+
+}
